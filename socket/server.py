5bcccb011882a18b5ad167c02cfd580e087f5c19
Bhavin Raichura <65218241+BhavinRaichura@users.noreply.github.com>
2022-01-28 21:13:29 +0530

    Create server.py

---
+++ socket/server.py
@@ -0,0 +1,24 @@
+import socket
+
+HOST = socket.gethostnamebyname(socket.gethostname())
+
+PORT = 8000
+
+server= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server.bind((HOST,PORT))
+
+server.listen()
+
+client, address = server.accept()
+
+done = True
+
+
+while done:
+    msg = client.recv(1024).decode('utf-8')
+    if msg=='quit':
+        done = False
+    else:
+        print(msg)
+    client.send(input("message: ").encode('utf-8'))
