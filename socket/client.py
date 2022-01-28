773607f9bd3d77c02be33f0737fd41a6a2d93a88
BhavinRaichura <bhavinraichura28@gmail.com>
2022-01-28 21:26:32 +0530

    commit

---
+++ socket/client.py
@@ -0,0 +1,20 @@
+import socket
+
+HOST = socket.gethostnamebyname(socket.gethostname())
+
+PORT = 8000
+
+client= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client.connect((HOST,PORT))
+
+name = input("client name : ")
+
+done =True
+while done:
+    client.send(input("MESSAGE: ").encode('utf-8'))
+    msg = client.recv(1024).decode('utf-8')
+    if msg == 'quit':
+        done=False
+    else:
+        print(msg)
